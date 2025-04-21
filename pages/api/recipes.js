1d3698b8174ecc06a59bc5cd07c17d21a60a0d29
ananyadrew23 <ananyadrew20@gmail.com>
2025-04-21 14:57:48 +0700

    test

--- pages/api/recipes.js
+++ pages/api/recipes.js
@@ -1,29 +1,10 @@
-import fs from 'fs'
-import path from 'path'
+import { mysqlPool } from "@/utils/db";
+import { NextResponse } from "next/server";
 
-const filePath = path.join(process.cwd(), 'data', 'recipes.json')
-
-export default function handler(req, res) {
-  let recipes = JSON.parse(fs.readFileSync(filePath))
-  const { method } = req
-
-  if (method === 'GET') {
-    return res.status(200).json(recipes)
-  }
-
-  if (method === 'POST') {
-    const newRecipe = { ...req.body, id: Date.now() }
-    recipes.push(newRecipe)
-    fs.writeFileSync(filePath, JSON.stringify(recipes, null, 2))
-    return res.status(201).json(newRecipe)
-  }
-
-  if (method === 'DELETE') {
-    const { id } = req.body
-    recipes = recipes.filter(r => r.id !== id)
-    fs.writeFileSync(filePath, JSON.stringify(recipes, null, 2))
-    return res.status(200).json({ message: 'Deleted' })
-  }
-
-  res.status(405).end()
+export async function GET(request) {
+  const promisePool = mysqlPool.promise()
+  const [rows, fields] = await promisePool.query(
+    `SELECT * FROM attractions;`
+  )
+  return NextResponse.json(rows)
 }
